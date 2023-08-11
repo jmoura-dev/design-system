a8f7502126f5cd64a7354c9cb4eda7cec9e986c3
Jackson <jackson1712@hotmail.com>
2023-08-11 12:21:09 -0300

    refactor: change items to recive new features

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,32 @@
+import { ComponentProps } from 'react'
+
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$code',
   backgroundColor: '$ignite300',
-  borderRadius: '$lg',
-  padding: '$4',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Hello World!</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
