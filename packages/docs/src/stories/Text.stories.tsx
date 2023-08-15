d895ccef758499e38ebd534832ba85dc98790c3b
Jackson <jackson1712@hotmail.com>
2023-08-15 19:44:57 -0300

    feat: create new components with styles and your documents

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,44 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  tags: ['autodocs'],
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita, porro odio aut blanditiis debitis illo ab quod voluptates corrupti, voluptas autem numquam nihil incidunt earum dolorem minus? Distinctio, asperiores deleniti!',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Heading',
+    as: 'strong',
+  },
+}
