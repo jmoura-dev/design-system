6d7ca65f14c3da2826db8ed7eb68443c6e143b9a
Jackson <jackson1712@hotmail.com>
2023-08-16 18:45:47 -0300

    fix: add subpath to storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,4 +1,5 @@
 import { join, dirname } from "path";
+import { config } from "process";
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -28,5 +29,11 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system/'
+    }
+    return config
+  }
 };
 export default config;
