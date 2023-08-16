6812e8309c8d885b045e7d657034adc601112e99
Jackson <jackson1712@hotmail.com>
2023-08-16 19:23:45 -0300

    fix

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -5,14 +5,14 @@ const config = {
     "../src/stories/**/*.stories.tsx",
   ],
   addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-a11y"),
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: "@storybook/react-vite",
     options: {},
   },
   docs: {
